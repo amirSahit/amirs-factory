7af943f09509cbb7cd7b87dbcb789847c0f1ce88
Amir <amir.sahit@gmail.com>
2023-08-10 12:39:04 +0200

    motivation for mobile is done

--- src/components/home/Motivation.tsx
+++ src/components/home/Motivation.tsx
@@ -1,17 +1,36 @@
 import Header from "../Header";
 import { useTranslation } from "react-i18next";
+import { useMediaQuery } from "react-responsive";
+
 function Motivation() {
   const { t } = useTranslation();
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   return (
     <section className="fullHeight bg-blue" id="fullHeight">
       <Header />
-      <section className="flex flex-row justify-around items-center h-full">
-        <article className="bg-white p-4 w-[50vw]">
-          <h2 className="mediumHeader text-red">Motivation.</h2>
-          <p className="normalText p-2">{t("MotivationText")}</p>
-        </article>
-        <img src="static/images/IMG-0511.jpg" className="borderHeadshot w-96" />
-      </section>
+      {isMobile ? (
+        <section className="flex flex-row md:justify-around md:items-center md:h-full relative">
+          <article className="bg-white p-4 w-[50vw] min-w-[300px] ml-6">
+            <h2 className="mediumHeader text-red">Motivation.</h2>
+            <p className="normalText p-2">{t("MotivationText")}</p>
+          </article>
+          <img
+            src="static/images/IMG-0511.jpg"
+            className="borderHeadshot w-96 absolute bottom-[-7vh] right-[2vw]"
+          />
+        </section>
+      ) : (
+        <section className="flex flex-row md:justify-around md:items-center md:h-full">
+          <article className="bg-white p-4 w-[50vw] min-w-[300px] ml-6">
+            <h2 className="mediumHeader text-red">Motivation.</h2>
+            <p className="normalText p-2">{t("MotivationText")}</p>
+          </article>
+          <img
+            src="static/images/IMG-0511.jpg"
+            className="borderHeadshot w-96"
+          />
+        </section>
+      )}
     </section>
   );
 }
