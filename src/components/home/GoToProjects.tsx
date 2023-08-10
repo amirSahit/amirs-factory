8d88e9c948cfd6b1bc9c6f8ea0f8241909e383a6
Amir <amir.sahit@gmail.com>
2023-08-10 12:44:22 +0200

    GoTo Projects is done

--- src/components/home/GoToProjects.tsx
+++ src/components/home/GoToProjects.tsx
@@ -5,16 +5,16 @@ import { useTranslation } from "react-i18next";
 function GoToProjects() {
   const { t } = useTranslation();
   return (
-    <section className="fullHeight" id="fullHeight">
+    <section className="fullHeight flex flex-col" id="fullHeight">
       <Header />
-      <section className="flex flex-row justify-around items-center">
+      <section className="flex flex-row justify-around items-center mt-[15vh]">
         <article className="bg-lightRed p-4 w-[75vw] flex flex-col gap-[4vh]">
           <h2 className="largeHeader text-black">Work.</h2>
           <h2 className="largeHeader text-black">Build & Design.</h2>
           <h2 className="largeHeader text-black">TypeScript & React.</h2>
           <Link
             to="/work"
-            className="bg-white border-4 border-black w-[15vw] place-self-center px-15 py-5 text-center text-2xl font-bold"
+            className="bg-white border-4 border-black w-[15vw] min-w-[100px] place-self-center px-15 py-5 text-center md:text-2xl text-xl font-bold"
           >
             {t("GoToProjects")}
           </Link>
